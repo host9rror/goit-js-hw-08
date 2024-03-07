5b368ecb7391e323fa4267c4a8c674e32985684d
zero <sanyafullgit@gmail.com>
2024-03-07 17:00:45 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -69,23 +69,25 @@ const images = [
     ];
 
     images.forEach((imageSrc) => {
-      const galleryItem = document.createElement('div');
-      galleryItem.classList.add('gallery-item');
+      if (typeof imageSrc === 'string') {
+        const galleryItem = document.createElement('div');
+        galleryItem.classList.add('gallery-item');
   
-      const galleryImage = document.createElement('img');
-      galleryImage.classList.add('gallery-image');
-      galleryImage.setAttribute('src', imageSrc);
-      galleryImage.setAttribute('data-source', imageSrc);
+        const galleryImage = document.createElement('img');
+        galleryImage.classList.add('gallery-image');
+        galleryImage.setAttribute('src', imageSrc);
+        galleryImage.setAttribute('data-source', imageSrc);
   
-      galleryItem.appendChild(galleryImage);
-      galleryContainer.appendChild(galleryItem);
+        galleryItem.appendChild(galleryImage);
+        galleryContainer.appendChild(galleryItem);
+      } 
     });
   
     function modalOpen(event) {
       event.preventDefault();
   
-      if (event.target.closest('.gallery-image')) {
-        const largeImageSource = event.target.closest('.gallery-item').querySelector('.gallery-image').dataset.source;
+      if (event.target.classList.contains('gallery-image')) {
+        const largeImageSource = event.target.dataset.source;
   
         basicLightbox.create(`
           <img width="1400" height="900" src="${largeImageSource}">
